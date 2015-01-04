aef3412cca725faf0102ce52e1f9d64c1c5bf6cf
Nikolas Liolios <Nliolios@exeter.edu>
2015-01-04 13:06:19 -0800

    First Commit

---
+++ textrank.py
@@ -0,0 +1,38 @@
+import string
+import nltk
+import sys
+import networkx as nx
+import numpy as np
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+sentences = []
+for arg in sys.argv[1:]:
+	try:
+		sentences += tokenizer.tokenize(open(arg).read())
+	except Exception, e:
+		raise e
+
+def normalize(sentence):
+	sentence = sentence.lower()
+	tokenizer = RegexpTokenizer(r'\w+')
+	tokens = tokenizer.tokenize(sentence)
+	filtered_words = [w for w in tokens if not w in stopwords.words('english')]
+	return " ".join(filtered_words)
+
+def textrank(sentences):
+	matrix = CountVectorizer().fit_transform(sentences)
+	normalized = TfidfTransformer().fit_transform(matrix)
+	similarity_graph = normalized * normalized.T
+
+	nx_graph = nx.from_scipy_sparse_matrix(similarity_graph)
+	scores = nx.pagerank(nx_graph)
+
+	return sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+
+print textrank(sentences)
